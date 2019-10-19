6f100ed61e686ea5738e2dc93ecd7cf23cbfdffe
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-18 23:36:28 -0400

    attach semantic ui, add form elements and styling to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default class SearchBar extends React.Component {
    render() {
-     return <div>SearchBar</div>
+     return(
+       <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" />
+          </div>
+        </form>
+       </div>
+     )
    }
 }
