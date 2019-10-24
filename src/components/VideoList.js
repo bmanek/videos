bd82a7dac9c935ae4e52d30299d5bd64fee94095
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-24 18:52:45 -0400

    create and connect VideoItem component, set VideoList to render VideoItem

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem'
 
 const VideoList = ({ videos }) => {
-  return <div>There are {videos.length} videos!</div>
+  const renderedList = videos.map((video) => {
+    return <VideoItem />
+  })
+
+  return <div>{renderedList}</div>
 }
 
 export default VideoList
