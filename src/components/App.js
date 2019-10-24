b4405724aa4c984eb56bec8f4a2550d7b4ae95b7
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-23 21:00:28 -0400

    create video state in App.js, create setState call with videos

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@
  const API_KEY = process.env.REACT_APP_API_KEY
 
  class App extends React.Component {
+   state = { videos: [] }
 
    onTermSubmit = async term => {
      const response = await youtube.get('/search', {
@@ -13,13 +14,14 @@
        }
      })
 
-     console.log(response)
+     this.setState({ videos: response.data.items })
    }
 
    render() {
      return (
        <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        I have {this.state.videos.length} videos!
        </div>
      )
    }
