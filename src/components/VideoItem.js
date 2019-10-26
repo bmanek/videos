4eb6e63fa65159199fb273946d8c44e287e204dc
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-25 21:59:36 -0400

    render thumbnail, title in VideoItem

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const VideoItem = (props) => {
-  return <div>Whoa, lookat me!</div>
+const VideoItem = ({ video }) => {
+  return (
+    <div>
+      <img src={video.snippet.thumbnails.medium.url} />
+      {video.snippet.title}
+    </div>
+  )
 }
 
 export default VideoItem
