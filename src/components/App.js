0dac4d5eee98ca2ecf79d1a1d6a0bcdaeac2da15
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-26 14:42:06 -0400

    add, connect basic VideoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
  import React from 'react'
  import SearchBar from './SearchBar'
  import VideoList from './VideoList'
+ import VideoDetail from './VideoDetail'
  import youtube from '../apis/youtube'
 
  const API_KEY = process.env.REACT_APP_API_KEY
@@ -22,13 +23,14 @@
    }
 
    onVideoSelect = video => {
-     console.log("From the app!", video)
+     this.setState({ selectedVideo: video })
    }
 
    render() {
      return (
        <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos}
           onVideoSelect={this.onVideoSelect} />
        </div>
